8dd5cb164541a05d10438d87e259f34351b2ae3b
botirk38 <btrghstk@gmail.com>
2023-12-19 11:13:39 +0200

    Fix some bugs in server.c

--- server.c
+++ server.c
@@ -1,9 +1,9 @@
 #include "server.h"
 
+
 int main() {
     int port = 8080;
     SSL_CTX *ctx;
-
 
     int server_fd = initialize_server(port, &ctx);
     printf("Server initialized at port %d\n", port);
@@ -12,8 +12,8 @@ int main() {
     int addrLen = sizeof(address);
 
     mongoc_init();
-    uri = mongoc_uri_new("your_mongodb_uri");
-    pool = mongoc_client_pool_new(uri)
+    mongoc_uri_t *uri = mongoc_uri_new("localhost:5000"); 
+    mongoc_client_pool_t *pool = mongoc_client_pool_new(uri);
 
     while (1) {
         int client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrLen);
@@ -25,37 +25,38 @@ int main() {
         struct client_handling_args {
             int client_socket;
             mongoc_client_pool_t *mongo_pool;
-        }
+        };
 
-        
         struct client_handling_args *args = malloc(sizeof(struct client_handling_args));
-
-        if(!args){
-            perror("Failed to allocate memory for client sockets");
+        if (!args) {
+            perror("Failed to allocate memory for client handling arguments");
             close(client_socket);
             continue;
         }
 
-        args -> client_socket = client_socket;
-        arg -> mongo_client_pool_t = pool;
+        args->client_socket = client_socket;
+        args->mongo_pool = pool;
 
         pthread_t thread_id;
-        if (pthread_create(&thread_id, NULL, handleClient, args != 0) {
+        if (pthread_create(&thread_id, NULL, handleClient, args) != 0) {
             perror("Failed to create thread");
             close(client_socket);  // Close the client socket as thread creation failed
-            free(client_socket_ptr);  // Free the allocated memory
+            free(args);  // Free the allocated memory
             continue;
         }
 
         pthread_detach(thread_id);  // Detach the thread
     }
-    mongoc_client_pool_push(pool, mongo_client);
+
+    // Cleanup MongoDB resources
     mongoc_client_pool_destroy(pool);
     mongoc_uri_destroy(uri);
     mongoc_cleanup();
-    // Close the server socket
+
+    // Close the server socket and cleanup SSL context
     close(server_fd);
     SSL_CTX_free(ctx);
     EVP_cleanup();
     return 0;
 }
+
